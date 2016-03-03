ad4ffc21cf724b49f388e4f33d5a442a95659602
YouJinTou <dandreevd@gmail.com>
2016-03-03 19:37:43 +0200

    Tried completing Problem 55

--- 54. Poker Hands/54.cs
+++ 54. Poker Hands/54.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
-public class Card
+class Card
 {
     public Card(char rank, char suit)
     {
@@ -12,6 +13,22 @@ public class Card
 
     public char Rank { get; set; }
     public char Suit { get; set; }
+}
+
+class HandStrength
+{
+    public HandStrength()
+    {
+    }
+
+    public HandStrength(Enum handRanking, int kickerStrength)
+    {
+        this.HandRanking = handRanking;
+        this.KickerStrength = kickerStrength;
+    }
+
+    public Enum HandRanking { get; set; }
+    public int KickerStrength { get; set; }
 }
 
 class PokerTable
@@ -72,131 +89,233 @@ class PokerTable
             }
         }
 
-        var hand = new Card[2][];
-
-        hand[0] = playerOneHand;
-        hand[1] = playerTwoHand;
-
-        DetermineWinner(hand);
+        DetermineWinner(playerOneHand, playerTwoHand);
     }
 
-    public static void DetermineWinner(Card[][] hands)
+    public static void DetermineWinner(Card[] playerOneHand, Card[] playerTwoHand)
     {
-        Enum playerOneHandStrength = EvaluateHandStrength(hands[0]);
-        Enum playerTwoHandStrength = EvaluateHandStrength(hands[1]);
-
-        if (playerOneHandStrength.CompareTo(playerTwoHandStrength) > 0)
+        HandStrength playerOneHandStrength = EvaluateHandStrength(playerOneHand);
+        HandStrength playerTwoHandStrength = EvaluateHandStrength(playerTwoHand);
+        
+        if (playerOneHandStrength.HandRanking.CompareTo(playerTwoHandStrength.HandRanking) > 0)
         {
             playerOneWins++;
         }
-    }
-
-    public static Enum EvaluateHandStrength(Card[] hand)
-    {
-        if (IsRoyalFlush(hand))
+        else if (playerOneHandStrength.HandRanking.CompareTo(playerTwoHandStrength.HandRanking) == 0)
         {
-            return Rankings.RoyalFlush;
+            if (playerOneHandStrength.KickerStrength > playerTwoHandStrength.KickerStrength)
+            {
+                playerOneWins++;
+            }
         }
-        else if (IsStraightFlush(hand))
+    }
+
+    public static HandStrength EvaluateHandStrength(Card[] hand)
+    {
+        var handStrength = new HandStrength();
+        var handEvaluator = GetHandEvaluator(hand);
+
+        switch (handEvaluator.Count)
         {
-            return Rankings.StraightFlush;
+            case 2:
+                if (handEvaluator.Any(e => e.Value == 1))
+                {
+                    handStrength.HandRanking = Rankings.FourOfAKind;
+                    handStrength.KickerStrength = GetKickerStrength(handEvaluator, 1);
+
+                    return handStrength;
+                }
+                else
+                {
+                    handStrength.HandRanking = Rankings.FullHouse;
+                    handStrength.KickerStrength = GetKickerStrength(handEvaluator, 3);
+
+                    return handStrength;
+                }
+            case 3:
+                if (handEvaluator.Any(e => e.Value == 2))
+                {
+                    handStrength.HandRanking = Rankings.TwoPair;
+                    handStrength.KickerStrength = GetKickerStrength(handEvaluator, 2);
+
+                    return handStrength;
+                }
+                else
+                {
+                    handStrength.HandRanking = Rankings.ThreeOfAKind;
+                    handStrength.KickerStrength = GetKickerStrength(handEvaluator, 3);
+
+                    return handStrength;
+                }
+            case 4:
+                handStrength.HandRanking = Rankings.OnePair;
+                handStrength.KickerStrength = GetKickerStrength(handEvaluator, 2);
+
+                return handStrength;
+            case 5:
+                {
+                    bool isFlush = CardsAreOfSameSuit(hand);
+                    bool isStraight = CardsFormAStraight(hand);
+
+                    if (isFlush && isStraight)
+                    {
+                        if (hand.Any(c => c.Rank == 'T') && hand.Any(c => c.Rank == 'A'))
+                        {
+                            handStrength.HandRanking = Rankings.RoyalFlush;
+                            handStrength.KickerStrength = GetKickerStrength(handEvaluator, 0);
+
+                            return handStrength;
+                        }
+                        else
+                        {
+                            handStrength.HandRanking = Rankings.StraightFlush;
+                            handStrength.KickerStrength = GetKickerStrength(handEvaluator, 1);
+
+                            return handStrength;
+                        }
+                    }
+                    else if (isFlush)
+                    {
+                        handStrength.HandRanking = Rankings.Flush;
+                        handStrength.KickerStrength = GetKickerStrength(handEvaluator, 1);
+
+                        return handStrength;
+                    }
+                    else if (isStraight)
+                    {
+                        handStrength.HandRanking = Rankings.Straight;
+                        handStrength.KickerStrength = GetKickerStrength(handEvaluator, 1);
+
+                        return handStrength;
+                    }
+
+                    break;
+                }
         }
-        else if (IsFourOfAKind(hand))
+
+        handStrength.HandRanking = Rankings.HighCard;
+        handStrength.KickerStrength = GetKickerStrength(handEvaluator, 1);
+
+        return handStrength;
+    }
+
+    public static Dictionary<char, int> GetHandEvaluator(Card[] hand)
+    {
+        var handEvaluator = new Dictionary<char, int>();
+
+        foreach (var card in hand)
         {
-            return Rankings.FourOfAKind;
-        }
-        else if (IsFullHouse(hand))
-        {
-            return Rankings.FullHouse;
-        }
-        else if (IsFlush(hand))
-        {
-            return Rankings.Flush;
-        }
-        else if (IsStraight(hand))
-        {
-            return Rankings.Straight;
-        }
-        else if (IsThreeOfAKind(hand))
-        {
-            return Rankings.ThreeOfAKind;
-        }
-        else if (IsTwoPair(hand))
-        {
-            return Rankings.TwoPair;
-        }
-        else if (IsOnePair(hand))
-        {
-            return Rankings.OnePair;
+            if (!handEvaluator.ContainsKey(card.Rank))
+            {
+                handEvaluator[card.Rank] = 1;
+            }
+            else
+            {
+                handEvaluator[card.Rank]++;
+            }
         }
 
-        return Rankings.HighCard;
+        return handEvaluator;
     }
 
-    private static bool IsOnePair(Card[] hand)
+    public static bool CardsAreOfSameSuit(Card[] hand)
     {
-        throw new NotImplementedException();
-    }
+        var firstCardSuit = hand[0].Suit;
 
-    private static bool IsTwoPair(Card[] hand)
-    {
-        throw new NotImplementedException();
-    }
-
-    private static bool IsThreeOfAKind(Card[] hand)
-    {
-        throw new NotImplementedException();
-    }
-
-    private static bool IsStraight(Card[] hand)
-    {
-        throw new NotImplementedException();
-    }
-
-    private static bool IsFlush(Card[] hand)
-    {
-        throw new NotImplementedException();
-    }
-
-    private static bool IsFullHouse(Card[] hand)
-    {
-        throw new NotImplementedException();
-    }
-
-    private static bool IsFourOfAKind(Card[] hand)
-    {
-        throw new NotImplementedException();
-    }
-
-    public static bool IsStraightFlush(Card[] hand)
-    {
-        throw new NotImplementedException();
-    }
-
-    public static bool IsRoyalFlush(Card[] hand)
-    {
-        var royalStraight = new HashSet<char>() { 'T', 'J', 'Q', 'K', 'A' };
-        char firstSuit = hand[0].Suit;
-
-        for (int i = 0; i < hand.Length; i++)
+        for (int i = 1; i < hand.Length; i++)
         {
-            char currentSuit = hand[i].Suit;
-
-            if (currentSuit != firstSuit || !royalStraight.Contains(hand[i].Rank))
+            if (firstCardSuit != hand[i].Suit)
             {
                 return false;
             }
-
-            royalStraight.Remove(hand[i].Rank);
         }
 
         return true;
-    }    
+    }
+
+    public static bool CardsFormAStraight(Card[] hand)
+    {
+        char[][] possibleStraights = new char[][]
+        {
+            new char[] { 'A', '2', '3', '4', '5' },
+            new char[] { '2', '3', '4', '5', '6' },
+            new char[] { '3', '4', '5', '6', '7' },
+            new char[] { '4', '5', '6', '7', '8' },
+            new char[] { '5', '6', '7', '8', '9' },
+            new char[] { '6', '7', '8', '9', 'T' },
+            new char[] { '7', '8', '9', 'T', 'J' },
+            new char[] { '8', '9', 'T', 'J', 'Q' },
+            new char[] { '9', 'T', 'J', 'Q', 'K' },
+            new char[] { 'T', 'J', 'Q', 'K', 'A' }
+        };
+        bool isAStraight = true;
+
+        foreach (var straight in possibleStraights)
+        {
+            isAStraight = true;
+
+            foreach (var cardRank in straight)
+            {
+                if (!hand.Any(c => c.Rank == cardRank))
+                {
+                    isAStraight = false;
+                    break;
+                }
+            }
+        }
+
+        return isAStraight;
+    }
+
+    public static int GetKickerStrength(Dictionary<char, int> handEvaluator, int check)
+    {
+        int bestResult = 0;
+        int currentBest = 0;
+
+        foreach (var entry in handEvaluator)
+        {
+            if (entry.Value != check)
+            {
+                continue;
+            }
+
+            switch (entry.Key)
+            {
+                case 'T':
+                    currentBest = 58;
+                    break;
+                case 'J':
+                    currentBest = 59;
+                    break;
+                case 'Q':
+                    currentBest = 60;
+                    break;
+                case 'K':
+                    currentBest = 61;
+                    break;
+                case 'A':
+                    currentBest = 62;
+                    break;
+                default:
+                    currentBest = entry.Key;
+                    break;
+            }
+
+            if (bestResult < currentBest)
+            {
+                bestResult = currentBest;
+            }
+        }
+
+        return bestResult;
+    }
 
     static void Main()
     {
         playerOneWins = 0;
 
         ReadInput();
+
+        Console.WriteLine("Player one's total wins: {0}", playerOneWins);
     }
 }
