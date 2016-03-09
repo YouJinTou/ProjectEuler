e5895f794d1b86aed8c9456f5babf188b23bfdc8
YouJinTou <dandreevd@gmail.com>
2016-03-09 21:13:37 +0200

    Added 12. Large Sum

---
+++ 13. Large Sum/13.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+
+class Program
+{
+    static BigInteger SumNumbers()
+    {
+        BigInteger result = 0;
+        var fileName = "100 numbers.txt";
+
+        using (var sr = new StreamReader(fileName))
+        {
+            string line = sr.ReadLine();
+
+            while (line != null)
+            {
+                result += BigInteger.Parse(line);
+
+                line = sr.ReadLine();
+            }
+        }
+
+        return result;
+    }
+
+    static void Main()
+    {
+        var sum = SumNumbers().ToString();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(sum[i]);
+        }
+
+        Console.WriteLine();
+    }
+}
