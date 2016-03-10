b58ea6f6bd03d14c15cd787496fb46c6a3cbc501
YouJinTou <dandreevd@gmail.com>
2016-03-10 11:03:04 +0200

    Added 14. Longest Collatz Sequence

---
+++ 14. Longest Collatz Sequence/14.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static int GetCollatzSequenceTermCount(ulong firstElement)
+    {
+        Queue<ulong> collatzSequence = new Queue<ulong>();
+        int count = 0;
+
+        collatzSequence.Enqueue(firstElement);
+
+        while (collatzSequence.Count != 0)
+        {
+            ulong currentElement = collatzSequence.Dequeue();
+
+            if (currentElement == 1)
+            {
+                count++;
+
+                break;
+            }
+
+            if (currentElement % 2 == 0)
+            {
+                collatzSequence.Enqueue(currentElement / 2);
+            }
+            else
+            {
+                collatzSequence.Enqueue(currentElement * 3 + 1);
+            }
+
+            count++;
+        }
+
+        return count;
+    }
+
+    static void Main()
+    {
+        int bestCount = 0;
+
+        for (ulong i = 1; i < 1000000; i++)
+        {
+            int currentCount = GetCollatzSequenceTermCount(i);
+
+            if (currentCount > bestCount)
+            {
+                bestCount = currentCount;
+            }
+        }
+
+        Console.WriteLine(
+            "The number of terms for the longest Collatz sequence starting at under one million is {0}.", bestCount);
+    }
+}
