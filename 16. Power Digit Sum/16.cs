ef4998fe7265a75db030a1a2dbe0a9d055a14e15
YouJinTou <dandreevd@gmail.com>
2016-03-12 20:02:06 +0200

    Added 16. Power Digit Sum

---
+++ 16. Power Digit Sum/16.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BigInteger result = (BigInteger)Math.Pow(2, 1000);
+        var resultToString = result.ToString();
+        var sum = 0;
+
+        for (int i = 0; i < resultToString.Length; i++)
+        {
+            sum += (int)char.GetNumericValue(resultToString[i]);
+        }
+
+        Console.WriteLine("The sum of the digits is {0}.", sum);
+    }
+}
