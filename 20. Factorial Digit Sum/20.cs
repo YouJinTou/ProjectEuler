9768774be564f63465365a5a3f6ae4901a3ba0db
YouJinTou <dandreevd@gmail.com>
2016-03-13 22:18:20 +0200

    Added 20. Factorial Digit Sum

---
+++ 20. Factorial Digit Sum/20.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static BigInteger GetFactorial(int n)
+    {
+        BigInteger result = 1;
+
+        while (n >= 1)
+        {
+            result *= n;
+
+            n--;
+        }
+
+        return result;
+    } 
+
+    static void Main()
+    {
+        var factorialOneHundred = GetFactorial(100).ToString();
+        int result = 0;
+
+        foreach (var ch in factorialOneHundred)
+        {
+            result += (int)char.GetNumericValue(ch);
+        }
+
+        Console.WriteLine("The sum of all digits of 100! is {0}.", result);
+    }
+}
