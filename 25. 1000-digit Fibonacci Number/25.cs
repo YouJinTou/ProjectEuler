a444a59b2f5637ac69b6b10a4f8665f816cbaea6
YouJinTou <dandreevd@gmail.com>
2016-03-14 23:05:24 +0200

    Added 25. 1000-digit Fibonacci Number

---
+++ 25. 1000-digit Fibonacci Number/25.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static int GetFibIndex()
+    {
+        BigInteger prevFib = 1;
+        BigInteger currentFib = 1;
+        int index = 3;
+
+        while (!HasOneThousandDigits(currentFib))
+        {
+            BigInteger temp = currentFib;
+            currentFib += prevFib;
+            prevFib = temp;
+
+            index++;
+        }
+
+        return index--;
+    } 
+
+    static bool HasOneThousandDigits(BigInteger currentFib)
+    {
+        var numberToString = currentFib.ToString();
+
+        return numberToString.Length == 1000;
+    }
+
+    static void Main()
+    {
+        var fibIndex = GetFibIndex();
+
+        Console.WriteLine("The index is {0}.", fibIndex);
+    }
+}
